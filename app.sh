3063a354947af234fcbabaede22ea3c3caa05b6f
dhomas1 <dom@cotugno.co>
2025-04-21 10:54:15 -0400

    Update app.sh

--- app.sh
+++ app.sh
@@ -1,21 +1,4 @@
 ### UNRAR ###
-#_build_unrar() {
-#local VERSION="5.3.4"
-#local FOLDER="unrar"
-#local FILE="unrarsrc-${VERSION}.tar.gz"
-#local URL="http://www.rarlab.com/rar/${FILE}"
-
-#_download_tgz "${FILE}" "${URL}" "${FOLDER}"
-#pushd target/"${FOLDER}"
-#mv makefile Makefile
-#make CXX="${CXX}" CXXFLAGS="${CFLAGS} -D_FILE_OFFSET_BITS=64 -D_LARGEFILE_SOURCE" STRIP="${STRIP}" LDFLAGS="${LDFLAGS} -pthread"
-#make install DESTDIR="${DEST}"
-#mkdir -p "${DEST}/libexec"
-#mv "${DEST}/bin/unrar" "${DEST}/libexec/"
-#popd
-#}
-
-### UNRAR ###
 _build_unrar() {
 local VERSION="5.9.4"
 local FOLDER="unrar"
